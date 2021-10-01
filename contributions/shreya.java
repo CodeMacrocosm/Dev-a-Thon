319a371fa3968761128f07d513075762e848465d
Shreya Christiana <5hre9a@gmail.com>
2021-10-01 16:02:10 +0530

    shreya added

---
+++ contributions/shreya.java
@@ -0,0 +1,16 @@
+package contributions;
+public class shreya {
+    public static void main(String[] args) 
+    {
+         
+         
+        System.out.println("Hi, I am Shreya");
+         
+        System.out.println("I just love to learn about new languages and working on them");
+         
+        System.out.println("Coding gives me the confidence that I can solve complex technological issues as well");
+         
+        System.out.println("I look forward to learn new things and to increase my knowledge");
+     
+    }
+}
