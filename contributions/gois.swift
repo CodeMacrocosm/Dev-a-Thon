c0720bb311fb5990da428337f024b5089a4de7db
Matheus Gois <matheusgoislimasilva@gmail.com>
2021-10-02 12:23:25 -0300

    Create gois.swift

---
+++ contributions/gois.swift
@@ -0,0 +1,10 @@
+//
+//  Hello word.swift
+//  HacktoberFest
+//
+//  Created by Matheus Gois on 02/10/21.
+//  Copyright © 2021 Matheus Gois. All rights reserved.
+//
+
+print("Hy my name is Gois");
+print("I love so much coding <3");
