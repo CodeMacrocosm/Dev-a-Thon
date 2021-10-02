decce8791454f06cda8e066d30b989ff6f708ebc
Shreya Christiana <5hre9a@gmail.com>
2021-10-02 23:29:56 +0530

    Delete sheba.cpp

--- sheba.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main ( )
-{ 
- cout<<"Hi this is Sheba carolyn";
-cout<<" I love to contribute to open source community"; 
-cout<< " I love hacktoberfest ";
-
-}
