16bce7a7725f72ccfbf4211ebeb004de1292c1ed
praneeth2yadav <91823440+praneeth2yadav@users.noreply.github.com>
2021-10-03 14:26:18 +0530

    Create praneeth.cpp

---
+++ contributions/praneeth.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ( )
+{ 
+ cout<<"Hi this is Praneeth";
+cout<<" I love to contribute to open source community"; 
+cout<< " I love hacktoberfest ";
+return 0;
+}
