720654ca482647662b031eab6ffcadc713073d95
niharikaponugoti <71116379+niharikaponugoti@users.noreply.github.com>
2021-10-03 11:19:28 +0530

    Create Niha.cpp

---
+++ contributions/Niha.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ( )
+
+{ 
+
+ cout<<"Hi this is Shruthi";
+
+cout<<" I love to contribute to open source community"; 
+
+cout<< " I love hacktoberfest ";
+
+}
