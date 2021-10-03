3e4d14c342911b7386fb1e45ec4e3618509505f5
saumyakr1232 <saumyakr181999@gmail.com>
2021-10-03 15:07:52 +0530

    saumya

--- contributions/Saumya.java
+++ contributions/Saumya.java
@@ -1,5 +1,6 @@
 public class Saumya{
     public static void main(String[] args) {
-        System.out.println("Hello guys, My name is saumyma");
+        System.out.println("Hello guys, My name is saumya");
+        System.out.println("Hello guys, My name is saumya");
     }
 }
