62154fd5be52bf2448692dbf28574573d1fb341a
Pranjal <pranjalyadav1717@gmail.com>
2021-10-05 18:57:38 +0530

    Add pranjal.java

---
+++ contributions/pranjal.java
@@ -0,0 +1,11 @@
+
+public class pranjal {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello everyone! I'm Pranjal Yadav");
+		System.out.println("I love contributing to open source projects");
+
+	}
+	
+}
