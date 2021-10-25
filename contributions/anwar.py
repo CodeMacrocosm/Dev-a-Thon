ab14ae319037c7f5fbf089d8c3ac85c0148346e2
gracebewithyou <93127163+gracebewithyou@users.noreply.github.com>
2021-10-25 15:53:46 +0530

    Update anwar.py

--- contributions/anwar.py
+++ contributions/anwar.py
@@ -7,7 +7,7 @@ for letter in greet[1:]:
     space += 1
 
 print("\nI became (or am becoming) a programmer the shine a light in a dark room of numbers")
-print("Thanks for viewing and to Justen Phelps for creating the repo!")
+print("Thanks for viewing ")
 
 
 input()
