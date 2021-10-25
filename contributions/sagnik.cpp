87c82ae253d8ec9291fcbb5058f440d5eec60f83
Sagnik Sahoo <sahoosagnik1@gmail.com>
2021-10-25 12:35:14 +0530

    Create sagnik.cpp

---
+++ contributions/sagnik.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Everyone I am Sagnik Sahoo"<<endl;
+    cout<<"I love to contribute to the open source community and learn new things."<<endl;
+
+}
